76ca1a91c4bd27d49a1b65a5ce2f27b1a305e914
黄川 <huchuc@vip.qq.com>
2023-05-03 17:45:59 +0800

    抛出原始异常

--- nutz-dao-enhance-core/src/main/java/org/nutz/dao/enhance/util/ExceptionUtil.java
+++ nutz-dao-enhance-core/src/main/java/org/nutz/dao/enhance/util/ExceptionUtil.java
@@ -8,21 +8,21 @@ import java.lang.reflect.UndeclaredThrowableException;
  */
 public class ExceptionUtil {
 
-  private ExceptionUtil() {
-    // Prevent Instantiation
-  }
-
-  public static Throwable unwrapThrowable(Throwable wrapped) {
-    Throwable unwrapped = wrapped;
-    while (true) {
-      if (unwrapped instanceof InvocationTargetException) {
-        unwrapped = ((InvocationTargetException) unwrapped).getTargetException();
-      } else if (unwrapped instanceof UndeclaredThrowableException) {
-        unwrapped = ((UndeclaredThrowableException) unwrapped).getUndeclaredThrowable();
-      } else {
-        return unwrapped;
-      }
+    private ExceptionUtil() {
+        // Prevent Instantiation
     }
-  }
+
+    public static Throwable unwrapThrowable(Throwable wrapped) {
+        Throwable unwrapped = wrapped;
+        while (true) {
+            if (unwrapped instanceof InvocationTargetException) {
+                unwrapped = ((InvocationTargetException) unwrapped).getTargetException();
+            } else if (unwrapped instanceof UndeclaredThrowableException) {
+                unwrapped = ((UndeclaredThrowableException) unwrapped).getUndeclaredThrowable();
+            } else {
+                return unwrapped;
+            }
+        }
+    }
 
 }
